10d74b7081035e7f171e562a907d5db36f18e099
o66ns <tttrotzdem@gmail.com>
2025-08-12 02:50:11 +0300

    2

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = 'mongodb+srv://o66ns:GLl7FnyE@petfriend.p9bky34.mongodb.net/?retryWrites=true&w=majority&appName=petFriend'
 
 export const connectDB = async () => {
     if (!MONGO_URI) {
