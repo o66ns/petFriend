77b0373ee64e1743ecff825b6a806ed5ed030c0f
o66ns <tttrotzdem@gmail.com>
2025-08-12 02:06:06 +0300

    nf

--- petFriend/src/config.ts
+++ petFriend/src/config.ts
@@ -1,2 +1,3 @@
 //export const host = 'http://localhost:3000'
-export const host = 'http://192.168.0.100:3000'
+//export const host = 'http://192.168.0.100:3000'
+export const host = 'https://backend-w2cq.onrender.com'
