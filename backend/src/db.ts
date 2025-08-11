1f8fdc0e410d44882b0cf87ae3887bc503181b3a
o66ns <tttrotzdem@gmail.com>
2025-08-12 02:45:31 +0300

    1

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose'
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/petfriend'
+const MONGO_URI = process.env.MONGO_URI
 
 export const connectDB = async () => {
+    if (!MONGO_URI) {
+        console.error('No MONGO_URI in env')
+        process.exit(1)
+    }
+
     try {
         await mongoose.connect(MONGO_URI)
-        console.log('MongoDB підключено')
+        console.log('MongoDB connected')
     } catch (err) {
-        console.error('Помилка підключення до MongoDB:', err)
+        console.error('Error connecting MongoDB:', err)
         process.exit(1)
     }
 }
